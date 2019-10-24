cad16468ed3bb69d263e4ff9e873f1e453996651
Nicolas <nicolas.chauvigne@yahoo.fr>
2019-10-24 13:53:51 +0200

    Add animation
    + animation player
    + fix player

--- Assets/Script/Personnage.cs
+++ Assets/Script/Personnage.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 
 public class Personnage : MonoBehaviour
 {
+    public Animator animator;
     private string item = "";
     private bool isInAction = false;
     private bool selected;
@@ -35,17 +36,15 @@ public class Personnage : MonoBehaviour
                     RaycastHit hit;
                     
                     if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100) && hit.transform.gameObject.name != "Personnage") {
+                        animator.SetBool("isArrived", false);
+                        animToDirection(hit);
                         gameObject.GetComponent<NavMeshAgent>().destination = hit.point;
-                        if (hit.transform.gameObject.tag == "Element" && item == "" && isInAction == false)
-                        {
-                            isInAction = true;
+                        if (hit.transform.gameObject.tag == "Element" && item == "")
                             StartCoroutine(goPickElement(hit));
-                        }
-                        else if (hit.transform.gameObject.name == "Carton" && item != "" && isInAction == false)
-                        {
-                            isInAction = true;
+                        else if (hit.transform.gameObject.name == "Carton" && item != "")
                             StartCoroutine(goDropElement(hit));
-                        }
+                        else
+                            StartCoroutine(goToNewPosition(hit));
                     }
                 }
                 transform.localRotation = Quaternion.Euler(90, 0, 0);
@@ -53,9 +52,53 @@ public class Personnage : MonoBehaviour
         }
     }
 
+    void animToDirection(RaycastHit hit)
+    {
+        float angle = Vector3.Angle(hit.point - transform.position, hit.transform.forward);
+        
+        if (hit.point.x - transform.position.x <= 0) {
+            if (angle <= 45)
+                animator.SetBool("goForward", true);
+            else if (angle > 45 && angle <= 135)
+                animator.SetBool("goLeft", true);
+            else
+                animator.SetBool("goBackward", true);
+        } else {
+            if (angle <= 45)
+                animator.SetBool("goForward", true);
+            else if (angle > 45 && angle <= 135)
+                animator.SetBool("goRight", true);
+            else
+                animator.SetBool("goBackward", true);
+        }
+    }
+
+    IEnumerator goToNewPosition(RaycastHit hit)
+    {
+        if (gameObject.transform.position.x <= hit.point.x + 0.1 && gameObject.transform.position.x >= hit.point.x - 0.1 && gameObject.transform.position.z <= hit.point.z + 0.1 && gameObject.transform.position.z >= hit.point.z - 0.1) {
+            isInAction = true;
+            animator.SetBool("isArrived", true);
+            animator.SetBool("goForward", false);
+            animator.SetBool("goBackward", false);
+            animator.SetBool("goRight", false);
+            animator.SetBool("goLeft", false);
+            yield return new WaitForSeconds(1);
+            isInAction = false;
+        } else if (!isInAction) {
+            yield return new WaitForSeconds(0.1f);
+            StartCoroutine(goToNewPosition(hit));
+        }
+    }
+
     IEnumerator goPickElement(RaycastHit hit)
     {
         if (gameObject.transform.position.x <= hit.point.x + 0.3f && gameObject.transform.position.x >= hit.point.x - 0.3f && gameObject.transform.position.z <= hit.point.z + 0.3f && gameObject.transform.position.z >= hit.point.z - 0.3f) {
+            isInAction = true;
+            animator.SetBool("isArrived", true);
+            animator.SetBool("goForward", false);
+            animator.SetBool("goBackward", false);
+            animator.SetBool("goRight", false);
+            animator.SetBool("goLeft", false);
             gameObject.GetComponent<NavMeshAgent>().destination = transform.localPosition;
             yield return new WaitForSeconds(1);
             if (hit.transform.gameObject) {
@@ -64,7 +107,7 @@ public class Personnage : MonoBehaviour
                 Destroy(hit.transform.gameObject);
                 isInAction = false;
             }
-        } else {
+        } else if (!isInAction) {
             yield return new WaitForSeconds(0.25f);
             if (hit.transform.gameObject)
                 StartCoroutine(goPickElement(hit));
@@ -74,6 +117,12 @@ public class Personnage : MonoBehaviour
     IEnumerator goDropElement(RaycastHit hit)
     {
         if (gameObject.transform.position.x <= hit.point.x + 1.5 && gameObject.transform.position.x >= hit.point.x - 1.5 && gameObject.transform.position.z <= hit.point.z + 1.5 && gameObject.transform.position.z >= hit.point.z - 1.5) {
+            isInAction = true;
+            animator.SetBool("isArrived", true);
+            animator.SetBool("goForward", false);
+            animator.SetBool("goBackward", false);
+            animator.SetBool("goRight", false);
+            animator.SetBool("goLeft", false);
             gameObject.GetComponent<NavMeshAgent>().destination = transform.localPosition;
             yield return new WaitForSeconds(0.5f);
             if (hit.transform.gameObject) {
@@ -82,7 +131,7 @@ public class Personnage : MonoBehaviour
             }
             item = "";
             isInAction = false;
-        } else {
+        } else if (!isInAction) {
             yield return new WaitForSeconds(0.25f);
             if (hit.transform.gameObject.name == "Carton") {
                 StartCoroutine(goDropElement(hit));
