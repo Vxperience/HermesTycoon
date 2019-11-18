dd3b9867fc0c3cccdf82470c50cd683bb9540215
Nicolas <nicolas.chauvigne@yahoo.fr>
2019-11-18 15:33:27 +0100

    Responsive UI
    + Responsive UI;
    + bug fix UI;

---
+++ Assets/01_Scripts/Convoyeur.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Convoyeur : MonoBehaviour
+{
+    public Sprite[] sprite;
+    private int i;
+    private float speed;
+
+    void Start()
+    {
+        if (GameObject.Find("Niveau2"))
+            speed = 0.204f;
+        else
+            speed = 0.068f;
+        i = 0;
+        StartCoroutine(ChangeSprite());
+    }
+
+    IEnumerator ChangeSprite()
+    {
+        yield return new WaitForSeconds(speed);
+        gameObject.GetComponent<SpriteRenderer>().sprite = sprite[i + 1 >= sprite.Length ? i = 0 : i++];
+        StartCoroutine(ChangeSprite());
+    }
+}
